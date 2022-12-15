74752950d66f445947e4920b464e254cb419cfd4
Knightwinger <104516687+Knightwinger@users.noreply.github.com>
2022-12-15 17:27:46 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -9,6 +9,12 @@
     app.get("/", function(req, res){
         res.sendFile(__dirname + "/index.html");
     })
+    app.get("/products", function(req, res){
+        res.sendFile(__dirname + "/products.html");
+    })
+    app.get("/info", function(req, res){
+        res.sendFile(__dirname + "/info.html");
+    })
  
     //server port
     app.listen(process.env.PORT || 3003, function(){
